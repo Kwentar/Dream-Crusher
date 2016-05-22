190375966bc96828adf2481b44cc1d519d04cc2a
kwentar <alekseev.yeskela@gmail.com>
2016-05-22 21:09:25 +0300

    added Month models, edit base schema #DC-2 work development 1h20m added month document type

--- app/main_views.py
+++ app/main_views.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, g
 from time import gmtime, strftime
 from flask_login import login_required, current_user
+from app.models import Month, User
+import datetime
+
 
 main_module = Blueprint('main', __name__, template_folder='templates')
 
@@ -9,6 +12,19 @@ main_module = Blueprint('main', __name__, template_folder='templates')
 @login_required
 def index():
     current_month = strftime("%B", gmtime())
+    n_month = int(strftime("%m", gmtime()))
+    year = datetime.datetime.today().year
+    exist = False
+    for m in g.user.months:
+        if m.n_month == n_month and m.year == year:
+            exist = True
+            break;
+    if not exist:
+        month = Month(name=current_month, n_month=n_month, year=year)
+        month.save()
+        g.user.months.append(month)
+        g.user.save()
+
     return render_template('index.html', month=current_month)
 
 
--- app/models.py
+++ app/models.py
@@ -18,23 +18,42 @@ class Dream(db.Document):
         'ordering': ['title']
     }
 
+
+class Month(db.Document):
+    name = db.StringField(max_length=255, required=True)
+    year = db.IntField(min_value=datetime.datetime.today().year, required=True, default=datetime.datetime.today().year)
+    n_month = db.IntField(min_value=1, max_value=12, required=True)
+    slogan = db.StringField(max_length=640, required=True, default="I'm so lazy to come up with something")
+    dreams = db.ListField(db.EmbeddedDocumentField('Dream'))
+    meta = {
+        'indexes': ['n_month', 'year'],
+        'ordering': ['year', 'n_month']
+    }
+
+    def __unicode__(self):
+        return "{} of {} with slogan {}, Dreams: {}".format(
+            self.name,
+            self.year,
+            self.slogan,
+            ",".join([x.title for x in self.dreams]))
+
 ROLE_ADMIN = 0
 ROLE_USER = 1
 
 
 class User(db.Document, UserMixin):
     nickname = db.StringField(max_length=255, required=True)
-    email = db.StringField(max_length=255, required=True, unique=True, primary_key=True)
+    email = db.EmailField(max_length=255, required=True, unique=True, primary_key=True)
     role = db.IntField(required=True, default=ROLE_USER)
-    dreams = db.ListField(db.EmbeddedDocumentField('Dream'))
     avatar_url = db.StringField(required=True, defaul="")
+    months = db.ListField(db.EmbeddedDocumentField('Month'))
 
     def __unicode__(self):
-        return "{} {} {}, Dreams: {}".format(
+        return "{} {} {}, Months: {}".format(
             self.nickname,
             self.email,
             "user" if self.role else "admin",
-            ",".join([x.title for x in self.dreams]))
+            ",".join([x.title for x in self.months]))
 
     def get_id(self):
         return str(self.email)
