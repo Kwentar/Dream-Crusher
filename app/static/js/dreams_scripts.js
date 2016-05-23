99b3f4ea882e1bedf5de82ac064e7ffc2b6bcb8b
kwentar <alekseev.yeskela@gmail.com>
2016-05-23 03:44:28 +0300

    js sex #DC-8 work development work 1h13m

--- app/static/js/dreams_scripts.js
+++ app/static/js/dreams_scripts.js
@@ -1,24 +1,28 @@
-var $TABLE = $('#table');
-var $BTN = $('#export-btn');
-var $EXPORT = $('#export');
+$(document).ready(function() {
+  var $TABLE = $('#table');
 
 $('.table-add').click(function () {
-  var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
-  alert('aaaaa');
-  $TABLE.find('table').append($clone);
+    var $rowCount = $TABLE.find('tr').length;
+    if($rowCount < 5) {
+        var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
+        $TABLE.find('table').append($clone);
+    }
+    else {
+        alert("You can have only 3 dreams for one month, sorry =(")
+    }
 });
+  $('.table-remove').click(function () {
+    $(this).parents('tr').detach();
+  });
 
-$('.table-remove').click(function () {
-  $(this).parents('tr').detach();
-});
+  $('.table-up').click(function () {
+    var $row = $(this).parents('tr');
+    if ($row.index() === 1) return; // Don't go above the header
+    $row.prev().before($row.get(0));
+  });
 
-$('.table-up').click(function () {
-  var $row = $(this).parents('tr');
-  if ($row.index() === 1) return; // Don't go above the header
-  $row.prev().before($row.get(0));
-});
-
-$('.table-down').click(function () {
-  var $row = $(this).parents('tr');
-  $row.next().after($row.get(0));
+  $('.table-down').click(function () {
+    var $row = $(this).parents('tr');
+    $row.next().after($row.get(0));
+  });
 });
