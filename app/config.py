6d6e04c412767964587cd8ef45edc35a66c9f8c9
kwentar <alekseev.yeskela@gmail.com>
2016-06-02 15:34:38 +0300

    privates2

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ import os
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = "KeepThisInRlyS3cr3t"
-MONGODB_SETTINGS = {'DB': "DC_base"}
+MONGODB_SETTINGS = {'DB': "dream-crusher"}
+MONGO_URI = 'mongodb://mongo-user:BHDYyA2G@ds038379.mlab.com:38379/dream-crusher'
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
